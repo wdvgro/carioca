7b1cb4f83a147eed370f5288789085e55821c3f9
Mb Alhakim <57827232+Mbalhakim@users.noreply.github.com>
2023-12-05 10:08:42 +0100

    Delete TicTacToe/Versie-2 directory

--- TicTacToe/Versie-2/StatusButtonPanel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class StatusButtonPanel extends JPanel {
-    public StatusButtonPanel(JButton connectedButton) {
-        setLayout(new FlowLayout());
-        add(connectedButton);
-    }
-}
